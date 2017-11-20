ff903b5b9ef8e91a5367ec3ff08b1197916af849
aaronsalva27 <aaronsalva27@gmail.com>
2017-11-20 16:34:31 +0100

    toolbar added

--- app/src/main/java/fje/clot/daw2/sopadeletras/TableroActivity.java
+++ app/src/main/java/fje/clot/daw2/sopadeletras/TableroActivity.java
@@ -1,17 +1,18 @@
 package fje.clot.daw2.sopadeletras;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class TableroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tablero);
     }
 
     @Override
@@ -25,6 +26,4 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         return  true;
     }
-
-
 }
