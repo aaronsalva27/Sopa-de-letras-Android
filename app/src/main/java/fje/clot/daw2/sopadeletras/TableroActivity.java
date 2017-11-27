cefceebb26744e34a0f4f1e72e20a4e96263389f
aaronsalva27 <aaronsalva27@gmail.com>
2017-11-27 20:23:14 +0100

    table horizontal algorithm done

--- app/src/main/java/fje/clot/daw2/sopadeletras/TableroActivity.java
+++ app/src/main/java/fje/clot/daw2/sopadeletras/TableroActivity.java
@@ -3,6 +3,7 @@ package fje.clot.daw2.sopadeletras;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -19,12 +20,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
+import java.util.Arrays;
+
 import static android.R.attr.max;
 
 public class TableroActivity extends AppCompatActivity {
     private String[] palabras;
     private GridView tablero;
     ArrayAdapter<String> Adapter;
+    private TextView lbActual;
     static final char[] numbers = new char[] {
             'A', 'B', 'C', 'D', 'E',
             'F', 'G', 'H', 'I', 'J',
@@ -51,18 +56,29 @@ public class TableroActivity extends AppCompatActivity {
         String missatge = intent.getStringExtra(MainActivity.EXTRA_MISSATGE);
 
         tablero = (GridView) findViewById(R.id.grid);
+        lbActual = (TextView) findViewById(R.id.lbActual);
+
         generarTaba();
 
     }
 
     public void generarTaba(){
-        String[] letras = new String[90];
+        String[] letras;
+        ArrayList<char[]> filasParsed;
+        ArrayList<String>  sopa = new ArrayList<String>();
 
-        for (int i = 0; i<letras.length;i++){
-            letras[i] = String.valueOf(numbers[(int) (Math.random()*25+1)]);
+        filasParsed = generarSopaDeLetras();
+
+        System.out.println("TAMAÑO: "+filasParsed.size());
+        System.out.println(filasParsed.get(0)[0]);
+
+        for(int i = 0; i<filasParsed.size();i++) {
+            for (int j = 0 ; j<10 ;j++) {
+                sopa.add(String.valueOf(filasParsed.get(i)[j]));
+            }
         }
 
-        generarSopaDeLetras();
+        letras = sopa.toArray(new String[sopa.size()]);
 
         Adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, letras);
@@ -70,8 +86,9 @@ public class TableroActivity extends AppCompatActivity {
 
     }
 
-    public void generarSopaDeLetras(){
+    public ArrayList<char[]> generarSopaDeLetras(){
         ArrayList<char[]> filas = new ArrayList<char[]>();
+
 
         // creamos 9 arrays
         for(int i = 0 ; i<9; i++){
@@ -83,11 +100,11 @@ public class TableroActivity extends AppCompatActivity {
                 for (int j = 0; j < palabras[i].length(); j++) {
                     filas.get(i)[j] = (palabras[i].toCharArray()[j]);
                 }
-
             }else {
                 rellenarArray(filas.get(i),palabras[i]);
             }
         }
+
         for (int i = 0; i < filas.size(); i++){
             for (int j = 0 ; j < filas.get(i).length ;j++){
                 System.out.print(filas.get(i)[j]);
@@ -96,27 +113,23 @@ public class TableroActivity extends AppCompatActivity {
             System.out.println(" ");
         }
 
+        return filas;
     }
 
     public void rellenarArray(char[] fila,String palabra){
 
         // posibles combinaciones
-        int posicionesPosibles = 10-fila.length+1;
+        int posicionesPosibles = 10-palabra.length()+1;
         // posicion inicio
         int randomNum = (int) (Math.random()*posicionesPosibles+1);
-
-        System.out.println(palabra.toUpperCase());
-        System.out.println("POSSIBLES"+posicionesPosibles);
 
         // rellenamos con palabras random;
         for (int i = 0; i<fila.length;i++){
             fila[i] = numbers[(int) (Math.random()*25+1)];
         }
 
-        for (int i = randomNum; i<fila.length;i++){
-            for (int j = 0; j < palabra.length(); j++) {
-                fila[i] = (palabra.toCharArray()[j]);
-            }
+        for (int i = 0; i< palabra.length(); i++) {
+            fila[randomNum-1+i] = palabra.toCharArray()[i];
         }
 
     }
